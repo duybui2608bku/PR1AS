04bf0e9122cc2582b3685b2f2210d7fbf18d9a59
Bui Nhat Duy <duy.bui2608@hcmut.edu.vn>
2025-11-18 22:51:40 +0700

    clean

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,18 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  // Thêm một đối tượng cấu hình để ghi đè các quy tắc.
+  {
+    // Cấu hình các quy tắc cụ thể cho TypeScript
+    rules: {
+      // Tắt quy tắc cấm sử dụng kiểu 'any' tường minh.
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+
+  // Ghi đè các thư mục/file bỏ qua mặc định của eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
+    // Các thư mục bỏ qua mặc định:
     ".next/**",
     "out/**",
     "build/**",
